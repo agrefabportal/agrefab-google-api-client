353e2d856300aba808dee25e784f3536331cd425
Nate <nw3455@gmail.com>
2021-09-27 07:58:24 -0700

    Update documentation

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Agrefab Google API Client for NodeJS
-NodeJS client for Google APIs. Needs to be run on a server with a persistent filesystem due to tokens being saved as files.
+NodeJS client for getting data from Google APIs.
+
+## Installation
+`npm i agrefab-google-api-client`
+
+## How to use
+```javascript
+let api = new GoogleApiClient(MOCK_CREDENTIALS);
+let authResult = await api.authorize();
+let data = await api.listExampleData().catch(error => {
+    console.log('GOOGLE API RESPONSE ERROR: ' + error);
+});
+```
+See [tests.js](tests.js) for more examples.
+
+## Usage information
+* You must call and get a successful response from api.authrorize() before making a call to any API.
+*  The program needs to be run on a server with a persistent filesystem due to tokens being saved as files.
+* Requires valid credentials to be created in Google Cloud console. Make sure to enable all APIs that you plan to use.
+
+### Google APIs used
+* Google Sheets
--- test.js
+++ test.js
@@ -1,6 +1,3 @@
-const assert = require('assert')
-const fs = require('fs');
-const GoogleApiClient = require('./index.js');
 // WARNING: Remove client secret, client id, and project id before committing to version control. 
 var MOCK_CREDENTIALS = {
     "web": {
@@ -20,57 +17,65 @@ var MOCK_CREDENTIALS = {
         ]
     }
 };
-// WARNING: Remove this entire token before committing to version control. 
+// WARNING: Remove access token before committing to version control. 
 var MOCK_TOKEN = { "access_token": "###REMOVED###", "scope": "https://www.googleapis.com/auth/spreadsheets.readonly", "token_type": "Bearer", "expiry_date": 1632197422807 };
+const assert = require('assert');
+const fs = require('fs');
+const GoogleApiClient = require('./index.js');
 (async function main() {
-    await MOCK_WRITE_TOKEN_FILE();
+    await WRITE_TOKEN_FILE();
     Promise.all([
         await testListExampleData_returnsListOfItems(),
         await testGetGuide_returnsAgrefabGuide(),
     ]).then(async function (value) {
         console.log('🏖  All tests passed. ✅');
     });
-    await MOCK_DELETE_TOKEN_FILE();
+    await DELETE_TOKEN_FILE();
 })().catch(error => console.error(error));
 /**
  * Test get guide returns all the neccessary fields for an Appsheet guide to be converted into a PDF
  */
 async function testGetGuide_returnsAgrefabGuide() {
     try {
-        let api = new GoogleApiClient(MOCK_CREDENTIALS);
-        let authResult = await api.authorize();
-        assert.equal(authResult, true, "Mock credentials weren't valid. Any calls to a live API in this test won't work.");
-        let tables = await api.getGuide().catch(function (error) { console.log('GOOGLE API RESPONSE ERROR: ' + error); });
+        let api = await getGoogleApiClient();
+        let tables = await api.getGuide().catch(error => {
+            console.log('GOOGLE API RESPONSE ERROR: ' + error);
+        });
         assert.equal(tables.length > 0, true);
-        return;
-    } catch (error) {
-        console.log(error);
-    }
+    } catch (error) { console.log(error); }
 }
 /**
  * Test api call to a known public spreadsheet
  */
 async function testListExampleData_returnsListOfItems() {
     try {
-        let api = new GoogleApiClient(MOCK_CREDENTIALS);
-        let authResult = await api.authorize();
-        assert.equal(authResult, true, "Mock credentials weren't valid. Any calls to a live API in this test won't work.");
-        let data = await api.listExampleData().catch(function (error) { console.log('GOOGLE API RESPONSE ERROR: ' + error); });
+        let api = await getGoogleApiClient();
+        let data = await api.listExampleData().catch(error => {
+            console.log('GOOGLE API RESPONSE ERROR: ' + error);
+        });
         assert.equal(data.length > 0, true);
-    } catch (error) {
-        console.error(error);
-    }
+    } catch (error) { console.error(error); }
+}
+/**
+ * Get an instance of the google api client
+ * @returns {GoogleApiClient} api Google API Client
+ */
+async function getGoogleApiClient() {
+    let api = new GoogleApiClient(MOCK_CREDENTIALS);
+    let authResult = await api.authorize();
+    assert.equal(authResult, true, "Mock credentials weren't valid. Any calls to a live API in this test won't work.");
+    return api;
 }
 /**
  * Write mock token file
  */
-async function MOCK_DELETE_TOKEN_FILE() {
+async function DELETE_TOKEN_FILE() {
     fs.unlink('tokens.json', () => { });
 }
 /**
  * Write mock token file
  */
-async function MOCK_WRITE_TOKEN_FILE() {
+async function WRITE_TOKEN_FILE() {
     return new Promise((resolve, reject) => {
         fs.writeFile('tokens.json', JSON.stringify(MOCK_TOKEN), 'utf8', function MOCK_TOKEN_CALLBACK(error) {
             if (error) return reject(error);
